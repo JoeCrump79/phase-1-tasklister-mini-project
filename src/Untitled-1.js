5850d13dcb80914634a85c118224d958c8d7f665
Joseph Crump <josephcrump79@yahoo.com>
2024-09-12 15:37:20 -0500

    phase-1-tasklister-mini-project

---
+++ src/Untitled-1.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// const form = document.getElementById ("create-task-form");
+// form.addEventListener ("submit", event => {
+//   event.preventDefault()
+//   const formData = event.target.elements;
+//   console.log (formData ["new-task-description"].value)
+//   console.log (event)
+//   const data = new FormData (event.target) 
+//   console.log (data)
+// }
+// )
+// });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+const main = document.querySelector("#main-content")
+console.log(main)
+
+
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  //have the user input
+  const newTask = document.querySelector("#new-task-description").value
+  // slap it on the document
+  // where?
+  taskList.innerHTML += `
+    <li> ${newTask} 
+      <button> X for delete </button> 
+    </li>
+    
+    `
+
+
+  //const taskItem = document.createElement("li")
+  //taskItem.innerText = newTask 
+  //taskList.appendChild(taskItem)
+  taskForm.reset()
+})
+
